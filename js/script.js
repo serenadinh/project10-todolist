53e6657cba8e157c58484b4c81fdf1099fb96d42
Dinh <seredinh@34363bce4f2a.ant.amazon.com>
2018-10-22 13:22:00 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+//Todo list 
+function todoList() {
+	var item = document.getElementById("todoInput").value
+	var text = document.createTextNode(item)
+	var newItem = document.createElement("li")
+	newItem.appendChild(text)
+	document.getElementById("todoList").appendChild(newItem)
+}
